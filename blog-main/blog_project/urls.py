d681c382490533f0140308125aa0b36c8c145a3c
nnemiha <suskikovsuslik9@gmail.com>
2025-04-14 17:24:41 +0200

    commit

--- blog-main/blog_project/urls.py
+++ blog-main/blog_project/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Сначала свои кастомные views (например, signup)
+    path('accounts/', include('accounts.urls')),
+
+    # Потом стандартные Django auth (login/logout/password reset и т.д.)
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')), # новое добавление
+
     path('', include('blog.urls')),
 ]
+
